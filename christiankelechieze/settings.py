5b6ed9411d42311eecaff70825fef6ccc7bc83b8
christiankelechi <kezechristian@gmail.com>
2024-10-26 12:37:35 +0100

    updated my portfolio

--- christiankelechieze/settings.py
+++ christiankelechieze/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'baseapp',
     # demo
-    # 'whitenoise',
+    'whitenoise',
     # prod
     'whitenoise.runserver_nostatic',
 ]
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'christiankelechieze.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'baseapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'christiankelechieze.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -105,9 +105,9 @@ WSGI_APPLICATION = 'christiankelechieze.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse("postgres://kc:9IXtEnH4naXUEwG7QBGC8d4jdbewTV5y@dpg-cht1bc0rddlc2mfhkreg-a.oregon-postgres.render.com/christiankelechiezedb")
-    }
+# DATABASES = {
+#     'default': dj_database_url.parse("postgres://kc:9IXtEnH4naXUEwG7QBGC8d4jdbewTV5y@dpg-cht1bc0rddlc2mfhkreg-a.oregon-postgres.render.com/christiankelechiezedb")
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles'),]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
